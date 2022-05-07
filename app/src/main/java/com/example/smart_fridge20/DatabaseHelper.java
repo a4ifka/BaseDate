8da745842ca5124c08b225ae6742d0ef3f703187
Test <ilyha.borec@gmail.com>
2022-05-07 20:55:54 +0600

    BaseDateForSmartFridge

---
+++ app/src/main/java/com/example/smart_fridge20/DatabaseHelper.java
@@ -0,0 +1,35 @@
+package com.example.smart_fridge20;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "userstore.db"; // название бд
+    private static final int SCHEMA = 1; // версия базы данных
+    static final String TABLE = "users"; // название таблицы в бд
+    // названия столбцов
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_DESCRIPTION = "description";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, SCHEMA);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE users (" + COLUMN_ID
+                + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_NAME
+                + " TEXT, " + COLUMN_DESCRIPTION + " TEXT);");
+        // добавление начальных данных
+        db.execSQL("INSERT INTO "+ TABLE +" (" + COLUMN_NAME
+                + ", " + COLUMN_DESCRIPTION  + ") VALUES ('Картофель', '100шт');");
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,  int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE);
+        onCreate(db);
+    }
+}
