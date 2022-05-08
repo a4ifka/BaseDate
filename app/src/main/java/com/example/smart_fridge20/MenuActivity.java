99e045094264358079beefac81bf337b7d1f52d1
Romidorka <artemvasnev01@gmail.com>
2022-05-08 12:00:04 +0600

    Redesigned menu

--- app/src/main/java/com/example/smart_fridge20/MenuActivity.java
+++ app/src/main/java/com/example/smart_fridge20/MenuActivity.java
@@ -14,7 +14,7 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        Button btn = findViewById(R.id.btnProd);
+        Button btn = findViewById(R.id.product_list_btn);
 
 
         Intent intent = new Intent(this, MainActivity.class);
@@ -27,7 +27,7 @@ public class MenuActivity extends AppCompatActivity {
         });
 
 
-        Button btn2 = findViewById(R.id.btnSet);
+        Button btn2 = findViewById(R.id.product_buy_btn);
 
         Intent intent2 = new Intent(this,OrderActivity.class);
 
@@ -35,6 +35,14 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(intent2);
+            }
+        });
+
+        Button exit_btn = (Button) findViewById(R.id.exit_btn);
+        exit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
 
